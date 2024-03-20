87e568a1cfe8d48e1d44a9c0691afcee8ac3ccf3
Zifeng Chen <564520489@qq.com>
2024-03-20 16:05:12 +0800

    feat: 问卷列表页

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const questionList = [
+    { id: 'q1', title: '问卷1', isPublished: false },
+    { id: 'q2', title: '问卷2', isPublished: true },
+    { id: 'q3', title: '问卷3', isPublished: false },
+    { id: 'q4', title: '问卷4', isPublished: true },
+  ]
+  function edit(id: string) {
+    console.log('edit ', id)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>问卷列表页</h1>
+      <div>
+        {questionList.map(question => {
+          const { id, title, isPublished } = question
+          return (
+            <div key={id}>
+              <strong>{title}</strong>
+              &nbsp;
+              {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+              &nbsp;
+              <button
+                onClick={() => {
+                  edit(id)
+                }}
+              >
+                编辑问卷
+              </button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
